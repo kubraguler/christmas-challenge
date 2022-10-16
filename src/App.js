e6f91be935661811d4683f0d25d551e4e285bfa4
kubraguler <oktnkbr@gmail.com>
2022-10-16 19:02:11 +0100

    Santa component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Snowflake from './components/Snowflake/Snowflake'
+import ChristmasLights from './components/ChristmasLights/ChristmasLights'
+import SantaReindeer from './components/SantaReindeer/SantaReindeer'
 
 import './App.scss'
 
@@ -6,8 +8,25 @@ function App() {
 	return (
 		<div className='App'>
 			<Snowflake />
+			<ul className='lightrope'>
+				<ChristmasLights />
+			</ul>
+			<SantaReindeer />
 		</div>
 	)
 }
 
 export default App
+
+/**
+Santas reindeer names
+-Dasher
+-Dancer
+-Prancer
+-Vixen
+-Comet
+-Cupid
+-Donner
+-Blitzen
+- Rudolph
+*/
---
+++ src/components/SantaReindeer/SantaReindeer.js
@@ -0,0 +1,15 @@
+import './SantaReindeer.scss'
+
+const SantaReindeer = () => {
+	return (
+		<div className='santa-reindeer'>
+			<img
+				className='santa-smiles'
+				src='../../assets/images/santa-reindeer-sleigh.svg'
+				alt='santa smiles'
+			/>
+		</div>
+	)
+}
+
+export default SantaReindeer
