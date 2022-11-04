4bcc57b5ceb04595520e996ac93b2cb76dfc861b
kubraguler <oktnkbr@gmail.com>
2022-11-04 16:50:17 +0000

    Added date component

--- src/components/Reindeer/Reindeer.js
+++ src/components/Reindeer/Reindeer.js
@@ -1,12 +1,39 @@
 import "./Reindeer.scss";
-import image from "../../assets/images/deer-jumping.svg";
+// import image from "../../assets/images/deer-jumping.svg";
 
-const Reindeer = () => {
+const CalculateDate = () => {
+	const currentDate = new Date();
+	const myDate = new Date();
+	myDate.setFullYear(2022, 11, 13);
+
+	let timeLeft = myDate - currentDate;
+	timeLeft = timeLeft / 1000;
+
+	let seconds = Math.floor(timeLeft % 60);
+	timeLeft = timeLeft / 60;
+
+	let minutes = Math.floor(timeLeft % 60);
+	timeLeft = timeLeft / 60;
+
+	let hours = Math.floor(timeLeft % 24);
+	let days = Math.floor(timeLeft / 24);
+
+	console.log(days + " days, " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds");
+
 	return (
-		<div className='reindeer'>
-			<img className='reindeer-icon' src={image} alt='reindeer' />
+		<div className='days'>
+			<b>Until my holiday: {days} days left!</b>
 		</div>
 	);
 };
 
-export default Reindeer;
+// const Reindeer = () => {
+// 	return (
+
+// 		<div className='reindeer'>
+// 			<img className='reindeer-icon' src={image} alt='reindeer' />
+// 		</div>
+// 	);
+// };
+
+export default CalculateDate;
