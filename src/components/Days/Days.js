6153fc2fdc1cd74a68217ea7bd61aadaa2e4e9c7
kubraguler <oktnkbr@gmail.com>
2022-12-02 23:57:39 +0000

    Add animation to the text

--- src/components/Days/Days.js
+++ src/components/Days/Days.js
@@ -1,29 +1,34 @@
-import "./Days.scss"
+import "./Days.scss";
 
 const CalculateDate = () => {
-	const currentDate = new Date()
-	const myDate = new Date()
-	myDate.setFullYear(2022, 11, 13)
+  const currentDate = new Date();
+  const myDate = new Date();
+  myDate.setFullYear(2022, 11, 13);
 
-	let timeLeft = myDate - currentDate
-	timeLeft = timeLeft / 1000
+  let timeLeft = myDate - currentDate;
+  timeLeft = timeLeft / 1000;
 
-	let seconds = Math.floor(timeLeft % 60)
-	timeLeft = timeLeft / 60
+  let seconds = Math.floor(timeLeft % 60);
+  timeLeft = timeLeft / 60;
 
-	let minutes = Math.floor(timeLeft % 60)
-	timeLeft = timeLeft / 60
+  let minutes = Math.floor(timeLeft % 60);
+  timeLeft = timeLeft / 60;
 
-	let hours = Math.floor(timeLeft % 24)
-	let days = Math.floor(timeLeft / 24)
+  let hours = Math.floor(timeLeft % 24);
+  let days = Math.floor(timeLeft / 24);
 
-	console.log(days + " days, " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds")
+  console.log(days + " days, " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds");
 
-	return (
-		<div className='days'>
-			<b>Until my holiday: {days} days left!</b>
-		</div>
-	)
-}
+  return (
+    <div className="days">
+      <span className="text text-1">Until</span>
+      <span className="text text-2"> my</span>
+      <span className="text text-3"> holiday:</span>
+      <span className="text text-4"> {days}</span>
+      <span className="text text-5"> days</span>
+      <span className="text text-6"> left!</span>
+    </div>
+  );
+};
 
-export default CalculateDate
+export default CalculateDate;
